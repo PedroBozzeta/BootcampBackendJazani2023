dcb11b2b166ebf64211b2b8f84d81d4bd924410f
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-01 16:06:22 -0500

    Adding Configuration and Maping of the Office Model

---
+++ Jazani.Infrastructure/Admins/Configurations/OfficeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Jazani.Domain.Admins.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Jazani.Infrastructure.Admins.Configurations
+{
+    public class OfficeConfiguration: IEntityTypeConfiguration<Office>
+    {
+        //Configuración de la entidad Office
+        public void Configure(EntityTypeBuilder<Office> builder)
+        {
+            builder.ToTable("office", "adm");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasColumnName("name");
+            builder.Property(x => x.Description).HasColumnName("description");
+            builder.Property(x => x.RegistrationDate).HasColumnName("registrationdate");
+            builder.Property(x => x.State).HasColumnName("state");
+        }
+    }
+}
--- Jazani.Infrastructure/Cores/Contexts/ApplicationDbContext.cs
+++ Jazani.Infrastructure/Cores/Contexts/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Jazani.Domain.Admins.Models;
+using Jazani.Infrastructure.Admins.Configurations;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
 
 namespace Jazani.Infrastructure.Cores.Contexts
 {
@@ -19,6 +20,17 @@ namespace Jazani.Infrastructure.Cores.Contexts
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DbConnection"));
+        }
+
+        #region "DbSet"
+        public DbSet<Office> Offices {  get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
         }
     }
 }
