02b82542b74a79fe766f865a975978c1b62eec23
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-01 15:23:08 -0500

    Adding DbConnection and Reference between API and Infrastructure

---
+++ Jazani.Infrastructure/Cores/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Jazani.Infrastructure.Cores.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //Las variables de tipo privado y de tipo lectura que serán inyectadas
+        ///suelen ser escritas con un "_" al inicio
+        private readonly IConfiguration _configuration;
+
+        //Creando constructor
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DbConnection"));
+        }
+    }
+}
